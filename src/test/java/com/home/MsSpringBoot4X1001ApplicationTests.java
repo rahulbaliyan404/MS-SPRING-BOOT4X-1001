5e70613931d90f7a00ce16075e83ceda84b3bc09
rahulbaliyan@gmail.com <rahulbaliyan@gmail.com>
2025-12-05 15:10:35 +0530

    removed test cases

--- src/test/java/com/home/MsSpringBoot4X1001ApplicationTests.java
+++ src/test/java/com/home/MsSpringBoot4X1001ApplicationTests.java
@@ -3,11 +3,11 @@ package com.home;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MsSpringBoot4X1001ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }

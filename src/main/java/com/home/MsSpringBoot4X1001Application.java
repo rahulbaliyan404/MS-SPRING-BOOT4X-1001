c72f1c274c21a46ac6cffec9b3d62ab898f83960
rahulbaliyan@gmail.com <rahulbaliyan@gmail.com>
2025-12-05 15:09:46 +0530

    first commit

---
+++ src/main/java/com/home/MsSpringBoot4X1001Application.java
@@ -0,0 +1,13 @@
+package com.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSpringBoot4X1001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSpringBoot4X1001Application.class, args);
+	}
+
+}
